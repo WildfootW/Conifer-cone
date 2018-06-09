a3b7f622489a318c7bb1cd9dce67459231e46f6a
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-06-09 09:17:42 +0800

    dirty code

--- php/control.php
+++ php/control.php
@@ -3,8 +3,17 @@ if(isset($_POST['update_status']))
 {
     $write_text = $_POST['new_value'];
     $edit_file = fopen("./status.html", 'w');
-    fwrite($edit_file, $write_text);
+    if($edit_file === false)
+    {
+        echo "FAILLLLED";
+    }
+    fwrite($edit_file, $write_text[0] . "\n", 2);
     fclose($edit_file);
+    echo " SUCCESS";
+}
+else
+{
+    echo "FAILED";
 }
 ?>
 <body>
