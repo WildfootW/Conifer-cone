8ba022a5ae7fe31e75184756e3b9f0e186b33882
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-06-09 08:43:41 +0800

    init

--- php/control.php
+++ php/control.php
@@ -1,2 +1,16 @@
-<?
+<?php
+if(isset($_POST['update_status']))
+{
+    $write_text = $_POST['new_value'];
+    $edit_file = fopen("./status.html", 'w');
+    fwrite($edit_file, $write_text);
+    fclose($edit_file);
+}
+?>
+<body>
+<form method="post" action="control.php" id="update_form">
+    <input type="text" name="new_value">
+    <input type="submit" value="Update status" name="update_status">
+</form>
+</body>
 
